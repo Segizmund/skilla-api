f12ee8dc23fc1808231a92d7793c045316a223e6
Максим Сын Отца Своего <59877317+Segizmund@users.noreply.github.com>
2025-03-24 20:42:23 +0300

    24.03

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -21,20 +21,25 @@ function AudioPlayer({idAudio, id }) {
                 onEnded={() => setIsPlaying(false)}
             />
             <button onClick={togglePlay}>
-                <div className={'p-2 rounded-full bg-white'}>
-                    {isPlaying ?
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#002CFB"
-                             className="bi bi-pause-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
-                        </svg>
-                        :
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#002CFB"
-                             className="bi bi-caret-right-fill" viewBox="0 0 16 16">
-                            <path
-                                d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
-                        </svg>
-                    }
+                <div className={'bg-[#EAF0FA] p-3 rounded-[48px] flex items-center'}>
+                    <div className={'p-2 rounded-full bg-white'}>
+                        {isPlaying ?
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#002CFB"
+                                 className="bi bi-pause-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5m5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5"/>
+                            </svg>
+                            :
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#002CFB"
+                                 className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                            </svg>
+                        }
+                    </div>
+                    <div>
+                        <progress></progress>
+                    </div>
                 </div>
             </button>
         </div>
